9407d0b339ed445de28a3d4509fba3014a382f36
alwin <alpriven@gmail.com>
2024-12-05 16:40:20 +0200

    created solution that runs in O(n^2)

--- day1/solution.py
+++ day1/solution.py
@@ -7,16 +7,28 @@ def parse_input(input_file: Path) -> Tuple[List[int], List[int]]:
     second_list: List[int] = list()
     for line in input_file.read_text().splitlines():
         first_number, second_number = line.split()
-        first_list.append(first_number)
-        second_list.append(second_number)
+        first_list.append(int(first_number))
+        second_list.append(int(second_number))
     
     return first_list, second_list
 
 
 def main():
     input_file = Path(Path(__file__).parent, 'input.txt')
-    parsed_input = parse_input(input_file)
-    print(parsed_input)
+    first_list, second_list = parse_input(input_file)
+    total_distance = 0
+    for _ in range(min(len(first_list), len(second_list))):
+        first_min = min(first_list)
+        second_min = min(second_list)
+
+        first_list.remove(first_min)
+        second_list.remove(second_min)
+
+        total_distance += abs(first_min - second_min)
+    
+    print(total_distance)
+
+    
 
 
 if __name__ == '__main__':
