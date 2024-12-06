3db7479f3cc1f0b8aec99139f0674e9b9e0eedbf
alwin <alpriven@gmail.com>
2024-12-06 14:32:58 +0200

    added check for do/dont

--- day3/solution.py
+++ day3/solution.py
@@ -3,7 +3,9 @@ import re
 from typing import Iterable
 
 
-VALID_INSTRUCTION = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
+VALID_INSTRUCTION = re.compile(r"(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))")
+DO_INSTRUCTION = "do()"
+DONT_INSTRUCTION = "don't()"
 
 
 def parse_input(input_file: Path) -> Iterable[re.Match]:
@@ -14,8 +16,14 @@ def main():
     input_file = Path(Path(__file__).parent, "input.txt")
     matches = parse_input(input_file)
     total = 0
+    should_count = True
     for match in matches:
-        total += int(match.group(1)) * int(match.group(2))
+        if match.group(0) == DO_INSTRUCTION:
+            should_count = True
+        elif match.group(0) == DONT_INSTRUCTION:
+            should_count = False
+        else:
+            total += int(match.group(2)) * int(match.group(3)) if should_count else 0
 
     print(total)
 
