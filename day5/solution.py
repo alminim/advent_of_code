d50a5ed063a91739a202e3e735ad2df1abf631c2
alwin <alpriven@gmail.com>
2024-12-08 14:13:41 +0200

    all possible comparisons already exist so no need to create all the pairs in the list

--- day5/solution.py
+++ day5/solution.py
@@ -14,23 +14,12 @@ def create_order(order_rules: List[str]) -> None:
         ORDER.append(tuple(rule.split("|")))
 
 
-def create_all_pairs(update: str) -> List[Tuple[str, str]]:
-    pages = update.split(",")
-    pairs: List[Tuple[str, str]] = list()
-    for i in range(len(pages)):
-        for j in range(i, len(pages)):
-            pairs.append((pages[i], pages[j]))
-
-    return pairs
-
-
 def check_update_matches_order(update: str) -> bool:
-    update_pairs = create_all_pairs(update)
-    for pair in update_pairs:
-        if (pair[1], pair[0]) in ORDER:
-            return False
-
-    return True
+    update_pages = update.split(",")
+    return all(
+        (update_pages[i], update_pages[i + 1]) in ORDER
+        for i in range(len(update_pages) - 1)
+    )
 
 
 def main():
