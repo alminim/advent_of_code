82e30438ca3910a77308973cf2185424d8bb259d
alwin <alpriven@gmail.com>
2024-12-10 18:05:17 +0200

    fixed bug from using dict

--- day7/solution.py
+++ day7/solution.py
@@ -1,39 +1,38 @@
 from itertools import product
 from pathlib import Path
-from typing import List, Dict, Set
+from typing import List, Dict, Set, Tuple
 from simpleeval import simple_eval
 
 
 VALID_OPERATORS = {"+", "*"}
 
 
-def parse_input(input_file: Path) -> Dict[int, List[str]]:
+def parse_input(input_file: Path) -> Tuple[int, List[str]]:
     equations = input_file.read_text().splitlines()
-    return {
-        int(equation.split(":")[0]): equation.split(":")[1].split()
+    return [
+        (int(equation.split(":")[0]), equation.split(":")[1].split())
         for equation in equations
-    }
+    ]
 
 
-def calculate_equation(values: List[str], operators: str) -> int:
+def calculate_equation(values: List[str], operators: Tuple[str, ...]) -> int:
     total = simple_eval(operators[0].join(values[:2]))
     values = values[2:]
     operators = operators[1:]
 
     for value in values:
-        total = simple_eval(str(total) + operators[0] + str(value))
+        total = simple_eval(str(total) + operators[0] + value)
         operators = operators[1:]
 
     return total
 
 
-def calculate_all_evaluations(values: List[str]) -> Set[int]:
+def can_evaluation_be_reached(desired_evaluation: int, values: List[str]) -> bool:
     total_operators = len(values) - 1
-    possible_evaluations: Set[int] = set()
     for operator_combination in product(VALID_OPERATORS, repeat=total_operators):
-        possible_evaluations.add(calculate_equation(values, operator_combination))
-
-    return possible_evaluations
+        if desired_evaluation == calculate_equation(values, operator_combination):
+            return True
+    return False
 
 
 def main():
@@ -41,8 +40,8 @@ def main():
     parsed_input = parse_input(input_file)
 
     total_of_valid_evaluations = 0
-    for desired_evaluation, values in parsed_input.items():
-        if desired_evaluation in calculate_all_evaluations(values):
+    for desired_evaluation, values in parsed_input:
+        if can_evaluation_be_reached(desired_evaluation, values):
             total_of_valid_evaluations += desired_evaluation
 
     print(total_of_valid_evaluations)
